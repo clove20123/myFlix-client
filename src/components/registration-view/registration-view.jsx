b19c46762aa26ec0c363295eda6fa2297d8b0e1f
clove20123 <79578949+clove20123@users.noreply.github.com>
2021-07-30 13:36:39 +0200

    fixed errors

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,8 +1,12 @@
 import React, { useState } from 'react';
 import './registration-view.scss';
 import axios from 'axios';
+
+
+
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
+
 import PropTypes from 'prop-types';
 
 export function RegistrationView(props) {
@@ -17,10 +21,10 @@ export function RegistrationView(props) {
   };
 
   axios.post('https://my-movie-api-20123.herokuapp.com/users', {
-    Username: username,
-    Password: password,
-    Email: email,
-    Birthday: birthday
+    Username: Username,
+    Password: Password,
+    Email: Email,
+    Birthday: Birthday
   })
   .then(response => {
     const data = response.data;
@@ -35,30 +39,30 @@ export function RegistrationView(props) {
     <Form className="RegistrationForm" onSubmit={handleSubmit}>
       <Form.Group controlId="formGroupUsername">
         <Form.Label>Username</Form.Label>
-        <Form.Control type="text" placeholder="Enter Username" value={username} autoComplete="username" onChange={e => setUsername(e.target.value)} minLength="5" required />
+        <Form.Control type="text" placeholder="Enter Username" value={Username} autoComplete="username" onChange={e => setUsername(e.target.value)} minLength="5" required />
         <Form.Control.Feedback type="invalid">Please provide a valid username at least 5 characters long.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupPassword">
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Enter Password" value={password} autoComplete="password" onChange={e => setPassword(e.target.value)} minLength="5" required />
+        <Form.Control type="password" placeholder="Enter Password" value={Password} autoComplete="password" onChange={e => setPassword(e.target.value)} minLength="5" required />
         <Form.Control.Feedback type="invalid">Please provide a valid password at least 5 characters long.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupEmail">
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Enter Email" value={email} autoComplete="email" onChange={e => setEmail(e.target.value)} required />
+        <Form.Control type="email" placeholder="Enter Email" value={Email} autoComplete="email" onChange={e => setEmail(e.target.value)} required />
         <Form.Control.Feedback type="invalid">Please provide a valid email address.</Form.Control.Feedback>
       </Form.Group>
       <Form.Group controlId="formGroupBirthdate">
         <Form.Label>Birthdate</Form.Label>
-        <Form.Control type="date" placeholder="00-00-0000" value={birthdate} onChange={e => setBirthdate(e.target.value)} required />
+        <Form.Control type="date" placeholder="00-00-0000" value={Birthday} onChange={e => setBirthday(e.target.value)} required />
         <Form.Control.Feedback type='invalid'>Please enter a valid birthday.</Form.Control.Feedback>
       </Form.Group>
       <span>
         <Button type="submit" onClick={handleSubmit}>Submit</Button>
-        {' '}
-        <Link to="/">
+    
+        
           <Button variant="secondary" type="button">Back</Button>
-        </Link>
+        
       </span>
     </Form>
   );
